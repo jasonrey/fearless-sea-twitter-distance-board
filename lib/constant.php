59c7540cadff6acc156db38462dcaa8c7edd1c49
Jason Rey <jasonrey@outlook.com>
2015-05-26 16:57:30 +0800

    Restored previous constants

--- lib/constant.php
+++ lib/constant.php
@@ -1,3 +1,10 @@
 <?php
 
 !defined('SERVER_EXEC') && die('No access.');
+
+define('API_KEY', '26SA5lP3dczV47Dy0V1yap221');
+define('API_SECRET', 'nTIjjSAxla1AACdCh4iz71CZUWY3xtn6NO8hVw0AV8EfPzRrDP');
+
+define('TWEET_HASHTAG', '#nikeplus');
+define('TWEET_MODE', 'recent');
+define('TWEET_COUNT', 100);
